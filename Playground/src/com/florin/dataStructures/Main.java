8bf775b70feb44d36fd6a0273ade38203575768d
FZAMFIR <FZAMFIR@USHY-vurmtO6evD.computacenter.com>
2021-08-11 15:44:42 +0300

    created my own hashtable implementation

---
+++ Playground/src/com/florin/dataStructures/Main.java
@@ -0,0 +1,23 @@
+package com.florin.dataStructures;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Employee janeJones = new Employee("Jane", "Jones", 123);
+		Employee johnDoe = new Employee("John", "Doe", 123);
+		Employee marySmith = new Employee("Mary", "Smith", 123);
+		Employee mikeWilson = new Employee("Mike", "Wilson", 123);
+		Employee billEnd = new Employee("Bill", "End", 123);
+		
+		SimpleHashTable ht = new SimpleHashTable();
+		ht.put("Jones", janeJones);
+		ht.put("Doe", johnDoe);
+		ht.put("Wilson", mikeWilson);
+		ht.put("Smith", marySmith);
+		
+		ht.printHashTable();
+		
+		System.out.println(ht.get("Wilson"));
+	}
+
+}
---
+++ Playground/src/com/florin/dataStructures/SimpleHashTable.java
@@ -0,0 +1,36 @@
+package com.florin.dataStructures;
+
+public class SimpleHashTable {
+	private Employee[] hashtable;
+	
+	public SimpleHashTable() {
+		hashtable = new Employee[10];
+	}
+	
+	private int hashKey(String key) {
+		return key.length() % hashtable.length;	
+	}
+	
+	public void put(String key, Employee employee) {
+		int hashedKey = hashKey(key);
+		if (hashtable[hashedKey] != null) {
+			System.out.println("Already an item at index " + hashedKey);
+		} 
+		else {
+			hashtable[hashedKey] = employee;
+		}
+	}
+	
+	public Employee get(String key) {
+		int hashedKey = hashKey(key);
+		return hashtable[hashedKey];
+	}
+	
+	public void printHashTable() {
+		for (int i =0; i < hashtable.length; i++) {
+			System.out.println(hashtable[i]);
+		}
+	}
+	
+	
+}
