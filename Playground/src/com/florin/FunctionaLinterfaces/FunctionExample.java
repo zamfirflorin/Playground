e4f2fb01213f28832c19480659f4695dfb8517f4
FZAMFIR <FZAMFIR@USHY-vurmtO6evD.computacenter.com>
2021-09-11 22:36:58 +0300

    Predicate, Function and Consumer methods

---
+++ Playground/src/com/florin/FunctionaLinterfaces/FunctionExample.java
@@ -0,0 +1,18 @@
+package com.florin.FunctionaLinterfaces;
+
+import java.util.function.Function;
+
+public class FunctionExample {
+
+	
+	static Function<String, String> function = name -> name.toUpperCase();
+	static Function<String, String> addSomeString = s -> s.concat("anotherString");
+	
+	public static void main(String[] args) {
+		System.out.println("Result is : " + function.apply("florin") + "\n");
+		System.out.println("Result of AND THEN : " + function.andThen(addSomeString).apply("florin"));
+		System.out.println("Result of compose : " + function.compose(addSomeString).apply("florin"));
+	}
+	
+	
+}
--- Playground/src/com/florin/FunctionaLinterfaces/PredicateAndConsumer.java
+++ Playground/src/com/florin/FunctionaLinterfaces/PredicateAndConsumer.java
@@ -2,6 +2,7 @@ package com.florin.FunctionaLinterfaces;
 
 import java.util.List;
 import java.util.function.BiConsumer;
+import java.util.function.BiPredicate;
 import java.util.function.Consumer;
 import java.util.function.Predicate;
 
@@ -9,6 +10,9 @@ public class PredicateAndConsumer {
 	
 	Predicate<Student> p1 = s -> s.getGradeLevel() >= 3;
 	Predicate<Student> p2 = s -> s.getGpa() >= 3.9;
+	
+	BiPredicate<Integer, Double> biPredicate = (gradeLevel, gpa) -> gradeLevel >= 2 && gpa >= 1.9;
+	
 	BiConsumer<String, List<String>> studentBiConsumer = (name, activities) -> System.out.println(name + " : " + activities);
 	
 	Consumer<Student> studentConsumer =  student -> { 
@@ -17,8 +21,14 @@ public class PredicateAndConsumer {
 		}
 	};
 	
+	Consumer<Student> studentConsumerWithBiConsumer =  student -> { 
+		if (biPredicate.test(student.getGradeLevel(), student.getGpa())) {
+			studentBiConsumer.accept(student.getName(), student.getActivities());
+		}
+	};
+	
 	public void printNameAndActivities(List<Student> students) {
-		students.forEach(studentConsumer);
+		students.forEach(studentConsumerWithBiConsumer);
 	}
 
 	public static void main(String[] args) {
