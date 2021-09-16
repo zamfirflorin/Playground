7bba82621777f6e2d1261170d686c311dcf2f85a
FZAMFIR <zamfirflorinstelian@yahoo.com>
2021-09-16 19:49:33 +0300

    more streams operations

--- Playground/src/com/functional/streamsOperations/StreamFilterExample.java
+++ Playground/src/com/functional/streamsOperations/StreamFilterExample.java
@@ -1,10 +1,39 @@
 package com.functional.streamsOperations;
 
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import com.functional.FunctionaLinterfaces.Student;
+import com.functional.FunctionaLinterfaces.StudentDataBase;
+import static java.util.stream.Collectors.toList;
+
 public class StreamFilterExample {
 
 	public static void main(String[] args) {
-		
 
+		filterStudents().forEach(System.out::println);
+		System.out.println();
+		filterStudentsFemale().forEach(System.out::println);
+	}
+
+	public static List<Student> filterStudents() {
+
+		return StudentDataBase.getAllStudents()
+				.stream()
+				.filter(s -> s.getGradeLevel() > 2)
+				.sorted(Comparator.comparing(Student::getName)).sorted(Comparator.comparing(Student::getGpa))
+				.collect(Collectors.toList());
+
+	}
+
+	public static List<Student> filterStudentsFemale() {
+
+		return StudentDataBase.getAllStudents()
+				.stream()
+				.filter(s -> s.getGender()
+				.equals("female"))
+				.collect(toList());
 	}
 
 }
---
+++ Playground/src/com/functional/streamsOperations/StreamReduceExample.java
@@ -0,0 +1,16 @@
+package com.functional.streamsOperations;
+
+public class StreamReduceExample {
+
+//	Reduce is a terminal operation used to reduce the contents of a stream to a single value
+//	It takes two parameteres as an input
+//		- first parameters - default or initial value
+//		- second parameter - BinaryOperator<T>
+	
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
