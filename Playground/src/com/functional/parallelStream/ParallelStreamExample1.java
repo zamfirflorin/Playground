cb79b73b8e3cfa08be9e88b6a857f5b397b5358e
FZAMFIR <zamfirflorinstelian@yahoo.com>
2021-09-22 11:11:19 +0300

    when not to use parallel streams example

--- Playground/src/com/functional/parallelStream/ParallelStreamExample1.java
+++ Playground/src/com/functional/parallelStream/ParallelStreamExample1.java
@@ -26,13 +26,14 @@ public class ParallelStreamExample1 {
 	    public static List<String> parallelStream(){
 
 	        long start = System.currentTimeMillis();
+	        
 	        List<String> studentActivities = StudentDataBase.getAllStudents()
 	                .parallelStream()
 	                .map(Student::getActivities) //Stream<List<String>>
 	                .flatMap(List::stream) //<Stream<String>
 	                .collect(toList());
 
-	        long duration = System.currentTimeMillis()-start;
+	        long duration = System.currentTimeMillis() - start;
 	        System.out.println("Duration in parallel stream : "+ duration);
 
 	        return studentActivities;
@@ -43,11 +44,9 @@ public class ParallelStreamExample1 {
 
 	    public static void main(String[] args) {
 
-
 	        System.out.println("sequentialStream : " + sequentialStream());
 
 	        System.out.println("parallelStream : " + parallelStream());
-
 
 	    }
 
