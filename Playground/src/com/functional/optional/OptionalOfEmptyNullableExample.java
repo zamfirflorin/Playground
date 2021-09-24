2da7d8204d99f5a56f839a18a78ec57608cc7c8b
FZAMFIR <zamfirflorinstelian@yahoo.com>
2021-09-24 17:30:04 +0300

    optional of and ofNullable methods example

--- Playground/src/com/functional/optional/OptionalOfEmptyNullableExample.java
+++ Playground/src/com/functional/optional/OptionalOfEmptyNullableExample.java
@@ -4,16 +4,25 @@ import java.util.Optional;
 
 public class OptionalOfEmptyNullableExample {
 
+	public static final String myString = "hello";
 	
 	public static void main(String[] args) {
 		
-		System.out.println(ofNullable().get());
+		System.out.println(ofNullable());
+		System.out.println(of());
 
 	}
 	
 	public static Optional<String> ofNullable() {
 		
-		Optional<String> stringOptional = Optional.ofNullable("Hello");
+		Optional<String> stringOptional = Optional.ofNullable(null);
+		return stringOptional;
+	}
+	
+	
+	public static Optional<String> of() {
+		
+		Optional<String> stringOptional = Optional.of(null);
 		return stringOptional;
 	}
 	
