997517522d8a448bd45c7df58b51a867e483055b
FZAMFIR <zamfirflorinstelian@yahoo.com>
2021-09-27 09:53:20 +0300

    created annotations

---
+++ Playground/src/com/reflection/annotations/Designations.java
@@ -0,0 +1,14 @@
+package com.reflection.annotations;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.TYPE)
+public @interface Designations {
+	
+	public Designation[] value();
+
+}
